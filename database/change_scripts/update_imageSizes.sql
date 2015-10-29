b36fb3581bfb1221d479b913cf40850ed107886d
Adam Gallagher <asgpng@gmail.com>
2015-10-29 13:15:59 -0400

    Update schema.

--- database/change_scripts/update_imageSizes.sql
+++ database/change_scripts/update_imageSizes.sql
@@ -3,7 +3,7 @@ source change_scripts/create_imageSizes.sql;
 LOAD DATA LOCAL INFILE
     './data/imageSizes.csv'
 INTO TABLE
-    `iw_api`.`users`
+    `iw_api`.`imageSizes`
 FIELDS TERMINATED BY
     ','
 LINES TERMINATED BY '\n';
--- database/change_scripts/update_pinTypes.sql
+++ database/change_scripts/update_pinTypes.sql
@@ -3,7 +3,7 @@ source change_scripts/create_pinTypes.sql;
 LOAD DATA LOCAL INFILE
     './data/pinTypes.csv'
 INTO TABLE
-    `iw_api`.`users`
+    `iw_api`.`pinTypes`
 FIELDS TERMINATED BY
     ','
 LINES TERMINATED BY '\n';
